6a85fa4069f64e837f2661d9a8e293fe8cc2b6e9
Carlos Rossi <carlitosrossi@gmail.com>
2026-03-06 16:39:12 -0300

    add API key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'ADD YOUR API KEY HERE';
+const API_KEY = 'YOUR_OPENWEATHER_API_KEY';
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 const cityInput = document.getElementById('city-input');
